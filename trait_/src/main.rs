427b059bb7975c46dc7f50d5ff55cc4df39d598b
yaoming00 <l1ng14@foxmail.com>
2022-01-31 23:14:17 +0800

    feat: add trait aprt

---
+++ trait_/src/main.rs
@@ -0,0 +1,63 @@
+#![allow(unused)]
+pub trait Summary {
+    // fn summarize(&self) -> String;
+    fn summarize(&self) -> String {     // 默认实现
+        String::from("Read more...")
+    }
+    // 默认实现允许调用相同特征中的其他方法，哪怕这些方法没有默认实现。
+}
+pub struct Post {
+    pub title: String,   // 标题
+    pub author: String,  // 作者
+    pub content: String, // 内容
+}
+
+impl Summary for Post {} // 此时 Post 调用时，会使用默认实现
+
+pub struct Weibo {
+    pub username: String,
+    pub content: String,
+}
+
+impl Summary for Weibo {
+    fn summarize(&self) -> String {
+        format!("{}发表了微博{}", self.username, self.content)
+    }
+}
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list.iter() {
+        if item > largest {
+            largest = &item;
+        }
+    }
+
+    &largest
+}
+
+fn main() {
+    let post = Post {
+        title: "Rust语言简介".to_string(),
+        author: "Sunface".to_string(),
+        content: "Rust棒极了!".to_string(),
+    };
+    let weibo = Weibo {
+        username: "sunface".to_string(),
+        content: "好像微博没Tweet好用".to_string(),
+    };
+
+    println!("{}", post.summarize());
+    println!("{}", weibo.summarize());
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
