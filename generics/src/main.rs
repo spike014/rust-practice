26b530c4ec133b4e250027e71c13cdb0c0f60c3b
yaoming00 <l1ng14@foxmail.com>
2022-02-11 00:30:10 +0800

    chore: add more about generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -9,6 +9,13 @@ struct Point<T> {
     y: T,
 }
 
+#[derive(Debug)]
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+// 泛型参数不宜过多，应该多考虑拆分结构体和减少代码复杂度
+
 impl<T> Point<T> {
     fn x(&self) -> &T {
         &self.x
@@ -26,11 +33,23 @@ fn main() {
     let p = Point { x: 5, y: 10 };
     println!("p.x = {}", p.x());
 
+    let p2 = Point2 {x:111, y: 22.1};
+    println!("{:?}", p2);
+
     println!("add i8: {}", add(2i8, 3i8));
     println!("add i32: {}", add(20, 30));
     println!("add f64: {}", add(1.23, 1.23));
 
     // let p = Point{x: 1, y :1.1}; 类型不匹配
+
+    let arr: [i32; 3] = [1, 2, 3];
+    display_array(arr);
+
+    let arr: [i32; 2] = [1, 2];
+    display_array(arr);
 }
 
 // <const N: usize> 基于值的泛型
+fn display_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
+    println!("{:?}", arr);
+}
