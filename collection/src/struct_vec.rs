09cc53a3d3aeeb063ac3ed7299964f5478408118
yaoming00 <l1ng14@foxmail.com>
2022-02-20 20:25:19 +0800

    feat: add more into vector

--- collection/src/struct_vec.rs
+++ collection/src/struct_vec.rs
@@ -25,3 +25,27 @@ fn main() {
         ip.display();
     }
 }
+
+// 枚举实现
+/*
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String)
+}
+fn main() {
+    let v = vec![
+        IpAddr::V4("127.0.0.1".to_string()),
+        IpAddr::V6("::1".to_string())
+    ];
+
+    for ip in v {
+        show_addr(ip)
+    }
+}
+
+fn show_addr(ip: IpAddr) {
+    println!("{:?}",ip);
+}
+
+*/
